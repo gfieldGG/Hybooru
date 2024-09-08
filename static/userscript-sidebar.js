fa8823d890471b5fb6d4640ebb51faa3fe4bad79
MAHF <github@gfield.de>
2024-09-08 18:52:43 +0200

    okokok all done now

--- static/userscript-sidebar.js
+++ static/userscript-sidebar.js
@@ -1,11 +1,11 @@
 (function() {
     'use strict';
 
-	const updateSelector = "div#root"
-	const selector = "div>div.namespace"; // all relevant elements
+    const updateSelector = "div#root"
+    const selector = "div>div.namespace"; // all relevant elements
 
-	const blocksCollapse = ["statistics","sources"];
-	const blocksHide = ["filename","hybooru"];
+    const blocksCollapse = ["statistics","sources"];
+    const blocksHide = ["filename","hybooru"];
 
     const style = document.createElement('style');
     style.innerHTML = `
@@ -23,46 +23,45 @@
             display: none;
         }
 
-		.hidden {
-			display: none;
-		}
+        .hidden {
+            display: none;
+        }
     `;
     document.head.appendChild(style);
 
     function processBlocks() {
-        const blocks = document.querySelectorAll(selector);
+        if (!window.location.pathname.startsWith('/posts/')) return;
 
-        blocks.forEach(item => {
-			// hide if empty
-			if (item.querySelector('div,p') === null) {
-				item.classList.add('hidden');
-			}
-			else {
-				const title = item.querySelector('b').textContent.toLowerCase();
+        document.querySelectorAll(selector).forEach(item => {
+            // hide if empty
+            if (item.querySelector('div,p') === null) {
+                item.classList.add('hidden');
+            }
+            else {
+                const title = item.querySelector('b').textContent.toLowerCase();
 
-				if (blocksHide.includes(title)) {
-					item.classList.add('hidden');
-				}
+                if (blocksHide.includes(title)) {
+                    item.classList.add('hidden');
+                }
 
-				else if (blocksCollapse.includes(title)) {
-					item.classList.add('collapsible');
-	            	// Collapse by default
-	            	item.classList.add('collapsed');
-	            	// Add click event to toggle the collapse/expand
-	            	item.addEventListener('click', function() {
-	                	item.classList.toggle('collapsed');
-	            	});
-				}
-			}
+                else if (blocksCollapse.includes(title)) {
+                    item.classList.add('collapsible');
+                    // Collapse by default
+                    item.classList.add('collapsed');
+                    // Add click event to toggle the collapse/expand
+                    item.addEventListener('click', function() {
+                        item.classList.toggle('collapsed');
+                    });
+                }
+            }
         });
-
     }
 
-	// == == == Detect added nodes / attach MutationObserver == == ==
+    // == == == Detect added nodes / attach MutationObserver == == ==
     if (document.querySelector(updateSelector)){
         // Check once initially
-		processBlocks();
-		// Watch for changes that could be new videos
+        processBlocks();
+        // Watch for changes that could be new videos
         (new MutationObserver(processBlocks)).observe(document.querySelector(updateSelector), {childList: true, subtree: true});
     }
 })();
